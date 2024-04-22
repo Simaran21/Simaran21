006a55f2cd9876d6829dc195ae2bf2202abe75e8
Simaran21 <167753747+Simaran21@users.noreply.github.com>
2024-04-22 14:50:58 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -1,5 +1,5 @@
-public class LinearSearchExample{    
-public static int linearSearch(int[] arr, int key){    
+public class linearsearch{    
+public static int linearsearch(int[] arr, int key){    
         for(int i=0;i<arr.length;i++){    
             if(arr[i] == key){    
                 return i;    
@@ -10,6 +10,6 @@ public static int linearSearch(int[] arr, int key){
     public static void main(String a[]){    
         int[] a1= {10,20,30,50,70,90};    
         int key = 50;    
-        System.out.println(key+" is found at index: "+linearSearch(a1, key));    
+        System.out.println(key+" is found at index: "+linearsearch(a1, key));    
     }    
-}    
+}; 
